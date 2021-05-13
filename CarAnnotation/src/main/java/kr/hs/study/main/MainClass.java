10eef7bcb6225a38ad9f22e3104088159abb676e
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-13 14:30:48 +0900

    Annotation 실습

---
+++ CarAnnotation/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,25 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.hs.study.beans.HyunDai;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+		
+		HyunDai c1 = ctx.getBean("c1", HyunDai.class);
+		System.out.println("가격: " + c1.money(1000));
+		c1.order();
+		System.out.println("name: " + c1.getName());
+		System.out.println("color: " + c1.getColor());
+		System.out.println("==============================================");
+		
+		ctx.close();
+
+	}
+
+}
